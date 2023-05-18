c33456ca6aee380b4bb4b25da1aac919c10fa24c
Tod Hansmann <githubpub@todandlorna.com>
2023-05-17 19:26:16 -0600

    fix: add dorelease.sh script

--- .github/scripts/dorelease.sh
+++ .github/scripts/dorelease.sh
@@ -16,10 +16,61 @@ usage() {
 }
 
 fullrun() {
+  # Build the semver-tags command based on inputs
+  COMMAND_STRING="./semver-tags run " # --github_action "
   if [[ "${DRYRUN}" == "true" ]]; then
-    echo "Executed fullrun with dryrun"
+    COMMAND_STRING+="--dry_run "
+  fi
+
+  # This just adds all the package directories to the flags
+  for dir in packages/*; do COMMAND_STRING+="--directories ${dir} "; done
+
+  RESULT=$($COMMAND_STRING)
+
+  # Parse the results out to get the versions we need to update and the release notes
+  PUBLISHED=$(yq -P ".New_release_published" <<< $RESULT)
+  NEW_TAGS=$(yq -P ".New_release_git_tag" <<< $RESULT)
+  LAST_TAGS=$(yq -P ".Last_release_version" <<< $RESULT)
+  JSON_RELEASE_NOTES=$(yq -P ".New_release_notes_json" <<< $RESULT)
+  RUNDATE=$(date +"%Y-%m-%d-%T")
+
+  PUBLISHED_ARRAY=($(echo $NEW_TAGS | tr "," "\n"))
+  NEW_TAGS_ARRAY=($(echo $NEW_TAGS | tr "," "\n"))
+  LAST_TAGS_ARRAY=($(echo $LAST_TAGS | tr "," "\n"))
+  # This makes a version specific
+  printf "$JSON_RELEASE_NOTES" > "release_notes/${RUNDATE}-release-notes.json"
+
+  # For every new tag, update the package.json file so we can npm publish or whatnot
+  for i in "${!NEW_TAGS_ARRAY[@]}"
+  do
+    IFS='/' read -r DIR NEW_TAG <<< ${NEW_TAGS_ARRAY[i]}
+    LAST_VERSION=${LAST_TAGS_ARRAY[i]}
+    NEW_VERSION=${NEW_TAG#*v}
+    
+    # Now update all the things
+    # We use the "ci:" prefix because it doesn't count as a version bump
+    # but we do need to tag all these and commit the changes. We could break this up to a second loop I guess.
+    if [[ "${DRYRUN}" == "true" ]]; then
+      echo "Would be changing version $LAST_VERSION to $NEW_VERSION in $PACKAGE_DIR$DIR/package.json"
+      echo "Would run :"
+      echo " > git add \"$PACKAGE_DIR$DIR/package.json\""
+      echo " > git commit -m \"ci: adding version ${NEW_TAG} to $PACKAGE_DIR$DIR/package.json\""
+      echo " > git tag ${NEW_TAGS_ARRAY[i]}"
+    else
+      echo "Changing version $LAST_VERSION to $NEW_VERSION in $PACKAGE_DIR$DIR/package.json"
+      sed -i.bak "s/$LAST_VERSION/$NEW_VERSION/" "$PACKAGE_DIR$DIR/package.json"
+      rm -rf "$PACKAGE_DIR$DIR/package.json.bak"
+      git add "$PACKAGE_DIR$DIR/package.json"
+      git commit -m "ci: adding version ${NEW_TAG} to $PACKAGE_DIR$DIR/package.json"
+      git tag ${NEW_TAGS_ARRAY[i]}
+    fi
+  done
+
+  
+  if [[ "${DRYRUN}" == "true" ]]; then
+    echo "Would git push here"
   else
-    echo "Executed fullrun without dryrun"
+    git push
   fi
 }
 
---
+++ release_notes/README.md
@@ -0,0 +1,5 @@
+# release_notes
+
+This folder contains the resulting release notes in JSON format for every CI/CD run.
+
+It is not intended to stay this way, just to collect historical results for future processing, probably by the same thing we make for processing them as they are generated, whenever we get to that. It will be on a per-package basis.
