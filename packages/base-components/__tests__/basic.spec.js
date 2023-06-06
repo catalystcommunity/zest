15aaadfb2388ab1ad4397e34111d1b24c8c70831
Tod Hansmann <githubpub@todandlorna.com>
2023-06-05 22:50:38 -0600

    fix: test BaseComponent in multiple scenarios

--- packages/base-components/__tests__/basic.spec.js
+++ packages/base-components/__tests__/basic.spec.js
@@ -1,8 +1,54 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach, beforeAll } from 'vitest';
+import {readFileSync} from 'fs';
+import {resolve} from 'path';
 import {BaseComponent, DataComponent, OpenShadowDataComponent} from '../src';
 
-describe('basic test', () => {
-    it('should get the hello', () => {
-        expect(BaseComponent()).toBe('Hello from BaseComponents');
-    })
+class TestBaseComponent extends BaseComponent {
+    constructor() {
+        super();
+    }
+    connectedCallback(){
+        super.connectedCallback();
+        this.render()
+    }
+    render() {
+        this.innerHTML = this.template
+    }
+}
+const html = readFileSync(resolve(__dirname, './basic.html'), 'utf8');
+
+describe('BaseComponent', () => {
+    beforeAll(() => {
+        customElements.define( 'base-component', TestBaseComponent );
+    });
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+    });
+
+    it("should initialize correctly", () => {
+        const base = new TestBaseComponent();
+        expect(base).toBeInstanceOf(BaseComponent);
+        expect(base).toBeInstanceOf(TestBaseComponent);
+    });
+
+    it("should have a paragraph with the right ID", () => {
+        const base = new TestBaseComponent();
+        base.connectedCallback() // We have to call this because we're not actually in the dom
+        base.innerHTML = base.template
+        const nameP = base.querySelector('#testParagraphId')
+        expect(nameP).not.toBeNull();
+        expect(nameP).not.toBeUndefined();
+        expect(nameP.innerHTML).toEqual('Test Template');
+        // const style = window.getComputedStyle(base)
+        // console.log(style)
+        // expect(style.marginTop).toEqual('3em');
+    });
+
+    it("should have the right overridden template", () => {
+        const base = document.getElementById('testOverridesComp');
+        const nameP = base.querySelector('#testSecondParagraphId');
+        expect(nameP).not.toBeNull();
+        expect(nameP).not.toBeUndefined();
+        expect(nameP.innerHTML).toEqual('Second Test Template');
+    });
 });
