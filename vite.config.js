443c6b148bdc8a6cb2b09d0a8c16f66443999c3f
Tod Hansmann <githubpub@todandlorna.com>
2025-04-22 21:43:35 -0600

    fix: update codeowners and package versions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
+    base: "/",
     server: {
         https: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000'
+            }
+        }
+    },
+    preview: {
+        https: true,
+
+        port: 4173,
+        allowedHosts: true, 
         proxy: {
             '/api': {
                 target: 'http://localhost:5000'
@@ -12,4 +25,4 @@ export default {
     plugins: [
         basicSsl()
     ]
-}
+})
